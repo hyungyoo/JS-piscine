4198444a7e0ac4f20ef9700173cf6a4c5517c974
hyungyoo <hjyoo901112@gmail.com>
2022-10-04 17:02:58 +0200

    update for recipe_async

--- JS/JS02/ex04/recipe_async.js
+++ JS/JS02/ex04/recipe_async.js
@@ -12,6 +12,7 @@ async function spaghetti (etape, temps) {
             console.log(`${etape} 실패`);
             await delay(temps);
             throw (`${etape} 실패`);
+            spaghetti(etape, temps);
         };
         console.log(`${etape} 성공`);
         return (etape);
@@ -20,23 +21,27 @@ async function spaghetti (etape, temps) {
 async function doA() {
     while (42) {
         try {
-            return spaghetti('A : 면삶기', 3000);
+            await spaghetti('A : 면삶기', 3000);
+            return 'A 끝';
         } catch (e) {
             console.log(e);
         }
     };
 }
-//B1. 브로콜리 대치기 - 1초 
-//B2. 마늘과 양파 볶기 - 2초 
-//B3. 배이컨과 햄 볶기 - 2초 
-//B4. 소스, 남은 야채 넣고 다같이 볶기
+
+// 잘못짰음
+// b2에서 실패하면, b1으로 돌아감
+// try catch를 하나하나의 단계에 넣어줘야함
+// 그냥 doA를 반복해서 작업하는게 좋았음.
+// 다음부터는 미리 생각해야겠다 
 async function doB() {
     while (42) {
         try {
-            await spaghetti('B - 1 : 브로콜리 대치기', 1000)
-                .then (()=> spaghetti('B - 2 :마늘과 양파 볶기', 2000))
-                .then (()=> spaghetti('B - 3 : 베이컨과 햄 볶기', 2000));
-            return spaghetti('B - 4 :소스 남은야채넣고 다같이 볶기');
+            await spaghetti('B - 1 : 브로콜리 대치기', 1000);
+            await spaghetti('B - 2 :마늘과 양파 볶기', 2000);
+            await spaghetti('B - 3 : 베이컨과 햄 볶기', 2000);
+            await spaghetti('B - 4 :소스 남은야채넣고 다같이 볶기');
+            return 'B 끝';
         } catch (e) {
             console.log(e);
         }
@@ -46,8 +51,8 @@ async function doB() {
 async function doC () {
     while (42) {
         try {
-            spaghetti('C : 면까지 넣고 다 같이 볶기', 3000);
-            return;
+            await spaghetti('C : 면까지 넣고 다 같이 볶기', 3000);
+            return 'C 끝';
         } catch (e) {
             console.log(e);
         }
@@ -55,7 +60,9 @@ async function doC () {
 }
 
 async function makeSpaghetti() {
-    Promise.all([doA(), doB()]).then(()=>doC());
+    const AandB = await Promise.all([doA(), doB()]).then(list=> list.join(' '));
+    console.log(AandB);
+    doC();
 }
 
 makeSpaghetti();
